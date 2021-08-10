2af5e303040b5ba0493987349970342f788ddca8
Rowayda <rowaidasayad@gmail.com>
2021-08-11 02:32:32 +0300

    first commit

---
+++ app/src/main/java/com/example/ratingappudemy/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.ratingappudemy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+
+public class MainActivity extends AppCompatActivity {
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.ratingBar);
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                int userRating = (int) rating;
+                switch (userRating) {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+        });
+    }
+}
